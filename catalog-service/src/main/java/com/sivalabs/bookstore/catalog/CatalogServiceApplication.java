a00ce73ec74edadaaa5a5aa00720102881d54a30
Subham Jaiswal <subhamjaiswalamell@gmail.com>
2025-03-17 01:41:32 -0700

    Trigger github actions logs added

--- catalog-service/src/main/java/com/sivalabs/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/sivalabs/bookstore/catalog/CatalogServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CatalogServiceApplication {
     public static void main(String[] args) {
+        System.out.println("CatalogServiceApplication");
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 }
