4b863f225ee6cae5a679ba5fb905bc243bf44bd2
Subham Jaiswal <subhamjaiswalamell@gmail.com>
2025-03-25 02:24:35 -0700

    Trigerring github flow

--- catalog-service/src/main/java/com/sivalabs/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/sivalabs/bookstore/catalog/CatalogServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class CatalogServiceApplication {
     public static void main(String[] args) {
+        System.out.println("CatalogServiceApplication");
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 }
