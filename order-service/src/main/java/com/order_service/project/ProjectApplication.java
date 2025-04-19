9d4c13e3febf2cc7837ef4a1ba14f641c64c1cf4
Subham Jaiswal <subhamjaiswalamell@gmail.com>
2025-04-19 07:05:41 -0700

    Fixed YML file

---
+++ order-service/src/main/java/com/order_service/project/ProjectApplication.java
@@ -0,0 +1,17 @@
+package com.order_service.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
+public class ProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+}
